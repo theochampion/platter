ba71ab0320b728b7df4bd4a722cf9b398adb9932
theochampion <contact.theochampion@gmail.com>
2018-04-01 13:42:12 +0200

    server: Add script execution feedback

--- server/server.js
+++ server/server.js
@@ -1,83 +1,92 @@
-const WebSocket = require('ws');
-const uuidv4 = require('uuid/v4');
+const WebSocket = require("ws");
+const uuidv4 = require("uuid/v4");
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-let devices = new Map()
-let devicesWS = new Map()
-let webs = new Map()
-
+let devices = new Map();
+let devicesWS = new Map();
+let webs = new Map();
 
 const broadcast = (socketMap, payload) => {
-    console.log("Broadcasting", payload)
-    for (let [id, ws] of socketMap) {
-        ws.send(JSON.stringify(payload))
-    };
-}
+  console.log("Broadcasting", payload);
+  for (let [id, ws] of socketMap) {
+    ws.send(JSON.stringify(payload));
+  }
+};
 
 const addDevice = (ws, req) => {
+  let device = {
+    status: "online",
+    ip: req.connection.remoteAddress,
+    name: req.headers.name,
+    desc: req.headers.desc,
+    scripts: JSON.parse(req.headers.scripts),
+    id: uuidv4()
+  };
 
-    let device = {
-        status: 'online',
-        ip: req.connection.remoteAddress,
-        name: req.headers.name,
-        desc: req.headers.desc,
-        id: uuidv4()
-    }
+  console.log("Device: " + device.id);
 
-    console.log("Device: " + device.id)
+  devices.set(device.id, device);
+  devicesWS.set(device.id, ws);
 
-    devices.set(device.id, device)
-    devicesWS.set(device.id, ws)
-
-
-    ws.on('close', ws => {
-        console.log("delete device")
-        devices.delete(device.id)
-        device.status = 'offline'
-        broadcast(webs, device)
+  ws.on("message", msg => {
+    const scriptRes = JSON.parse(msg);
+    console.log(scriptRes);
+    broadcast(webs, {
+      status: "script_update",
+      id: device.id,
+      scriptNb: scriptRes.scriptNb,
+      returnCode: scriptRes.returnCode
     });
-    broadcast(webs, device)
-}
+  });
+
+  ws.on("close", ws => {
+    console.log("delete device");
+    devices.delete(device.id);
+    device.status = "offline";
+    broadcast(webs, device);
+  });
+  broadcast(webs, device);
+};
 
 const addWebClient = (ws, req) => {
-    const id = uuidv4(); // assign a unique id to each client
-    console.log("Web: " + id)
+  const id = uuidv4(); // assign a unique id to each client
+  console.log("Web: " + id);
 
-    webs.set(id, ws)
+  webs.set(id, ws);
 
-    ws.on('close', ws => {
-        console.log("Removing web " + id);
-        webs.delete(id)
-    });
+  ws.on("close", ws => {
+    console.log("Removing web " + id);
+    webs.delete(id);
+  });
 
-    ws.on('message', msg => {
-        const scriptOrder = JSON.parse(msg)
-        console.log(scriptOrder)
+  ws.on("message", msg => {
+    const scriptOrder = JSON.parse(msg);
+    console.log(scriptOrder);
 
-        const deviceWS = devicesWS.get(scriptOrder.id)
-        deviceWS.send(scriptOrder.script)
+    const deviceWS = devicesWS.get(scriptOrder.id);
+    deviceWS.send(scriptOrder.scriptNb);
+  });
 
-    })
-    for (let [id, device] of devices) {
-        ws.send(JSON.stringify({
-            status: device.status,
-            id: device.id,
-            ip: device.ip,
-            desc: device.desc,
-            name: device.name
-        }))
-    }
-}
+  for (let [id, device] of devices) {
+    ws.send(
+      JSON.stringify({
+        status: device.status,
+        id: device.id,
+        ip: device.ip,
+        desc: device.desc,
+        scripts: device.scripts,
+        name: device.name
+      })
+    );
+  }
+};
 
-wss.on('connection', function connection(ws, req) {
-    req.headers.type === 'device' ? addDevice(ws, req) : addWebClient(ws, req)
+wss.on("connection", function connection(ws, req) {
+  req.headers.type === "device" ? addDevice(ws, req) : addWebClient(ws, req);
 });
 
-
-
-console.log("Server started on port 8080...")
-
+console.log("Server started on port 8080...");
 
 /*
 {
--- src/containers/DevicesList.container.js
+++ src/containers/DevicesList.container.js
@@ -1,88 +1,100 @@
-import React, { Component } from 'react';
-import { Card, Container } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Card, Container } from "semantic-ui-react";
 
-import DeviceCard from '../components/DeviceCard'
-
+import DeviceCard from "../components/DeviceCard";
 
 class DevicesList extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            devices: new Map()
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      devices: new Map()
+    };
 
+    this.handleExecute = this.handleExecute.bind(this);
+  }
 
-        this.handleExecute = this.handleExecute.bind(this)
-        this.handleScriptChange = this.handleScriptChange.bind(this)
+  updateDevices(device) {
+    this.setState(prevState => {
+      switch (device.status) {
+        case "online":
+          prevState.devices.set(device.id, device);
+          break;
+        case "offline":
+          prevState.devices.delete(device.id);
+          break;
+        case "script_update":
+          prevState.devices.get(device.id).scripts[
+            device.scriptNb
+          ].running = false;
+          break;
+      }
+      console.log("device", ...prevState.devices);
+      console.log("id", device.id);
+      console.log(
+        "nb",
+        prevState.devices.get(device.id).scripts[device.scriptNb]
+      );
 
-    }
+      return { devices: prevState.devices };
+    });
+  }
 
-    updateDevices(id, device, deleteDevice = false) {
-        this.setState(prevState => {
+  connectToWSServer() {
+    this.ws = new WebSocket("ws://localhost:8080/websocket");
 
-            deleteDevice ? prevState.devices.delete(device.id) : prevState.devices.set(device.id, device)
-            console.log("device", ...prevState.devices)
-            console.log("id", id)
+    this.ws.onopen = () => {
+      this.props.onConnectionChange(true);
+    };
+    this.ws.onclose = () => {
+      console.log("close");
 
-            return { devices: prevState.devices }
-        })
-    }
+      this.props.onConnectionChange(false);
+      setTimeout(() => {
+        console.log("trying to connect");
+        this.connectToWSServer();
+      }, 2000);
+    };
 
-    connectToWSServer() {
-        this.ws = new WebSocket('ws://localhost:8080/websocket');
+    this.ws.onmessage = msg => {
+      console.log("msg", msg.data);
 
-        this.ws.onopen = () => {
-            this.props.onConnectionChange(true)
-        };
-        this.ws.onclose = () => {
-            console.log("close")
+      const device = JSON.parse(msg.data);
+      this.updateDevices(device);
+    };
+  }
 
-            this.props.onConnectionChange(false)
-            setTimeout(() => { console.log("trying to connect"); this.connectToWSServer() }, 2000)
-        };
+  handleExecute(id, scriptNb) {
+    console.log("executing script " + scriptNb + " on device " + id);
 
-        this.ws.onmessage = msg => {
-            console.log("msg", msg.data)
+    this.setState(
+      prevState => (prevState.devices.get(id).scripts[scriptNb].running = true)
+    );
+    this.ws.send(JSON.stringify({ id: id, scriptNb }));
+  }
 
-            const device = JSON.parse(msg.data)
-            this.updateDevices(device.id, device, device.status === "offline")
-        }
-    }
+  componentDidMount() {
+    this.connectToWSServer();
+  }
 
-    handleScriptChange(id, script) {
-        console.log({ ...this.state.devices.get(id), script: script })
-        this.updateDevices(id, { ...this.state.devices.get(id), script: script })
-    }
-
-    handleExecute(id) {
-        const device = this.state.devices.get(id)
-        if (device.script) {
-            console.log("executing script " + device.script + " on " + device.id)
-            this.ws.send(JSON.stringify({ id: id, script: device.script }))
-        }
-    }
-
-    componentDidMount() {
-        this.connectToWSServer()
-    }
-
-    render() {
-        return (
-            <Container >
-                <Card.Group>
-                    {[...this.state.devices].map(device => <DeviceCard
-                        key={device[1].id}
-                        id={device[1].id}
-                        name={device[1].name}
-                        ip={device[1].ip}
-                        desc={device[1].desc}
-                        script={device[1].script}
-                        onScriptChange={this.handleScriptChange}
-                        onExecute={this.handleExecute} />)}
-                </Card.Group>
-            </Container >
-        );
-    }
+  render() {
+    return (
+      <Container>
+        <Card.Group>
+          {[...this.state.devices].map(device => (
+            <DeviceCard
+              key={device[1].id}
+              id={device[1].id}
+              name={device[1].name}
+              ip={device[1].ip}
+              desc={device[1].desc}
+              scripts={device[1].scripts}
+              onExecute={this.handleExecute}
+            />
+          ))}
+        </Card.Group>
+      </Container>
+    );
+  }
 }
 
 export default DevicesList;
