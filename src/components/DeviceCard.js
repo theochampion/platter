d226e9532e48312cb1260f54db4907d88d071c66
theochampion <contact.theochampion@gmail.com>
2018-04-01 20:48:28 +0200

    DEviceCard: Extract script lit in component

--- src/components/DeviceCard.js
+++ src/components/DeviceCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ScriptList from "./ScriptList";
 import { Button, Card, List, Loader } from "semantic-ui-react";
 
 const DeviceCard = ({ id, scripts, name, ip, desc, onExecute }) => (
@@ -9,28 +10,7 @@ const DeviceCard = ({ id, scripts, name, ip, desc, onExecute }) => (
       <Card.Description>{desc}</Card.Description>
     </Card.Content>
     <Card.Content extra>
-      <List divided selection>
-        {scripts.map((script, idx) => (
-          <List.Item key={idx}>
-            <Button
-              loading={script.running}
-              basic
-              size="mini"
-              onClick={evt => {
-                onExecute(id, idx);
-              }}
-              floated="right"
-            >
-              Execute
-            </Button>
-            <List.Content
-              floated="left"
-              header={script.name}
-              description={script.desc}
-            />
-          </List.Item>
-        ))}
-      </List>
+      <ScriptList scripts={scripts} onExecute={onExecute} id={id} />
     </Card.Content>
   </Card>
 );
---
+++ src/components/ScriptList.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Button, List, Loader } from "semantic-ui-react";
+
+export default class ScriptList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedScript: null };
+  }
+
+  render() {
+    console.log(this.props);
+    const { onExecute, scripts, id } = this.props;
+    return (
+      <List divided selection>
+        {scripts.map((script, idx) => (
+          <List.Item
+            key={idx}
+            onClick={(evt, data) =>
+              this.setState({
+                selectedScript: this.state.selectedScript == idx ? null : idx
+              })
+            }
+          >
+            <List.Content floated="left">
+              <List.Header textAlign="left" content={script.name} />
+              {this.state.selectedScript == idx && (
+                <List.Description content={script.desc} />
+              )}
+            </List.Content>
+            <List.Content floated="right">
+              {this.state.selectedScript == idx && (
+                <Button
+                  color="teal"
+                  loading={script.running}
+                  basic
+                  size="mini"
+                  onClick={evt => {
+                    onExecute(id, idx);
+                  }}
+                  floated="right"
+                >
+                  Execute
+                </Button>
+              )}
+              {this.state.selectedScript != idx &&
+                script.running && <Loader active inline size="mini" />}
+            </List.Content>
+          </List.Item>
+        ))}
+      </List>
+    );
+  }
+}
